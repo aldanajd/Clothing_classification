866ffcd2d54b9ba659a5a023518635f3eaaa0d7b
Daniel Aldana <81043065+aldanajd@users.noreply.github.com>
2023-04-25 23:27:28 -0400

    Update classifier_app_st.py

--- classifier_app_st.py
+++ classifier_app_st.py
@@ -1,11 +1,16 @@
 import streamlit as st
 import numpy as np
-
+import os
 from tensorflow.keras.models import load_model
 from PIL import Image
 
+
+c = os.getcwd()
+f = list(os.walk(os.getcwd()))[0]
+d = os.path.join(c, f)
+
 #Load the pre-trained model
-classifier_model = load_model("/model/")
+classifier_model = load_model(d)
 
 #Define the categories to match the output of the pre-trained model
 classifier_dictionary = {
