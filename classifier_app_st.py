e14a5ffe360bd7d8d986a5485af85319a16710aa
Daniel Aldana <81043065+aldanajd@users.noreply.github.com>
2023-04-25 23:35:59 -0400

    Update classifier_app_st.py

--- classifier_app_st.py
+++ classifier_app_st.py
@@ -4,12 +4,9 @@ import os
 from tensorflow.keras.models import load_model
 from PIL import Image
 
-
 c = os.getcwd()
-
 d = os.path.join(c, 'model')
 
-st.write(d)
 #Load the pre-trained model
 classifier_model = load_model(d)
 
