c7c2c0328e5dddaac28d862dd8cb51c3ba9d428a
Daniel Aldana <81043065+aldanajd@users.noreply.github.com>
2023-04-25 22:16:14 -0400

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -1,4 +1,15 @@
-import os
+#import os
 
-l = list(os.walk(os.getcwd()))
-st.write(l)
+#l = list(os.walk(os.getcwd()))
+#st.write(l)
+
+import streamlit as st
+
+# Set page title
+st.set_page_config(page_title="Print Hello")
+
+# Add title to app
+st.title("Welcome to My Streamlit App!")
+
+# Print "hello" in Streamlit
+st.write("hello")
