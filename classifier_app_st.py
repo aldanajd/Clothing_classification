376830d8640f9b70d4a8c253404ec0025309f193
Daniel Aldana <81043065+aldanajd@users.noreply.github.com>
2023-04-25 23:09:42 -0400

    Update classifier_app_st.py

--- classifier_app_st.py
+++ classifier_app_st.py
@@ -5,7 +5,7 @@ from tensorflow.keras.models import load_model
 from PIL import Image
 
 #Load the pre-trained model
-classifier_model = load_model(r'/app/Clothing_classifier/Model')
+classifier_model = load_model(r'/app/Clothing_classifier/model')
 
 #Define the categories to match the output of the pre-trained model
 classifier_dictionary = {
